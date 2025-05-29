6ce98eefd1c39bae423048908eb17d59268df3d9
Alejandro Fernandez-Fraga <a.fernandez3@udc.es>
2025-05-29 02:12:42 +0200

    Steps towards emscripten compilation

--- src/006.BasicShaderSDL2/src/main.cpp
+++ src/006.BasicShaderSDL2/src/main.cpp
@@ -5,6 +5,51 @@
 #include <sstream>
 #include <chrono>
 
+#ifdef _EMSCRIPTEN
+#include <emscripten.h>
+#endif
+
+struct Context {
+    SDL_Window* window;
+    GLuint shader;
+    GLuint vao;
+    int currentWidth, currentHeight;
+    std::chrono::high_resolution_clock::time_point startTime;
+};
+
+#ifdef _EMSCRIPTEN
+void mainLoop(void* arg) {
+    Context *ctx = static_cast<Context *>(arg);
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            emscripten_cancel_main_loop();
+        }
+
+        // Handle window resize event
+        if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_RESIZED) {
+            SDL_GetWindowSize(ctx->window, &ctx->currentWidth, &ctx->currentHeight);
+            glViewport(0, 0, ctx->currentWidth, ctx->currentHeight);
+        }
+    }
+
+    float time = std::chrono::duration<float>(
+                     std::chrono::high_resolution_clock::now() - ctx->startTime)
+                     .count();
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(ctx->shader);
+    glUniform2f(glGetUniformLocation(ctx->shader, "iResolution"), 
+                static_cast<float>(ctx->currentWidth), 
+                static_cast<float>(ctx->currentHeight));
+    glUniform1f(glGetUniformLocation(ctx->shader, "iTime"), time);
+
+    glBindVertexArray(ctx->vao);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    SDL_GL_SwapWindow(ctx->window);
+}
+#endif
+
 std::string loadFile(const char* path) {
     std::ifstream file(path);
     std::stringstream buffer;
@@ -41,20 +86,35 @@ GLuint createProgram(const std::string& vsText, const std::string& fsText) {
 }
 
 int main() {
+
+    Context ctx;
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("Shader Viewer",
+    ctx.window = SDL_CreateWindow("Shader Viewer",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600,
         SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 
-    if (!window) {
+    if (!ctx.window) {
         std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
         return -1;
     }
+
+    #ifdef _EMSCRIPTEN
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    #else
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GLContext context = SDL_GL_CreateContext(window);
+    #endif
+    SDL_GLContext context = SDL_GL_CreateContext(ctx.window);
+    if (!context) {
+        std::cerr << "Failed to create OpenGL context: " << SDL_GetError() << std::endl;
+        return EXIT_FAILURE;
+    }
+    #ifndef _EMSCRIPTEN
     glewInit();
+    #endif
 
     const std::string shaderVert = R"""(
 #version 330 core
@@ -115,7 +175,7 @@ void main() {
 }
 )""";
 
-    GLuint shader = createProgram(shaderVert, shaderFragX);
+    ctx.shader = createProgram(shaderVert, shaderFragX);
 
     float vertices[] = {
         -1.0f, -1.0f,
@@ -123,10 +183,10 @@ void main() {
         -1.0f,  3.0f
     };
 
-    GLuint vao, vbo;
-    glGenVertexArrays(1, &vao);
+    GLuint vbo;
+    glGenVertexArrays(1, &ctx.vao);
     glGenBuffers(1, &vbo);
-    glBindVertexArray(vao);
+    glBindVertexArray(ctx.vao);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
@@ -136,13 +196,17 @@ void main() {
     SDL_Event event;
 
     // Set the initial viewport size
-    int currentWidth, currentHeight;
-    SDL_GetWindowSize(window, &currentWidth, &currentHeight);
-    glViewport(0, 0, currentWidth, currentHeight);
+    SDL_GetWindowSize(ctx.window, &ctx.currentWidth, &ctx.currentHeight);
+    glViewport(0, 0, ctx.currentWidth, ctx.currentHeight);
 
     // Start the timer
-    auto startTime = std::chrono::high_resolution_clock::now();
+    ctx.startTime = std::chrono::high_resolution_clock::now(); 
 
+
+    #ifdef _EMSCRIPTEN
+    emscripten_set_main_loop_arg(mainLoop, &ctx, 0, 1);
+    #endif
+    #ifndef _EMSCRIPTEN
     while (running) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT)
@@ -150,32 +214,33 @@ void main() {
 
             // Handle window resize event
             if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_RESIZED) {
-                SDL_GetWindowSize(window, &currentWidth, &currentHeight);
-                glViewport(0, 0, currentWidth, currentHeight);
+                SDL_GetWindowSize(ctx.window, &ctx.currentWidth, &ctx.currentHeight);
+                glViewport(0, 0, ctx.currentWidth, ctx.currentHeight);
             }
         }
 
         float time = std::chrono::duration<float>(
-        std::chrono::high_resolution_clock::now() - startTime).count();
+        std::chrono::high_resolution_clock::now() - ctx.startTime).count();
 
         // Crear the screen
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shader);
+        glUseProgram(ctx.shader);
 
-        glUniform2f(glGetUniformLocation(shader, "iResolution"), static_cast<float>(currentWidth), static_cast<float>(currentHeight));
-        glUniform1f(glGetUniformLocation(shader, "iTime"), time);
+        glUniform2f(glGetUniformLocation(ctx.shader, "iResolution"), static_cast<float>(ctx.currentWidth), static_cast<float>(ctx.currentHeight));
+        glUniform1f(glGetUniformLocation(ctx.shader, "iTime"), time);
         
-        glBindVertexArray(vao);
+        glBindVertexArray(ctx.vao);
         glDrawArrays(GL_TRIANGLES, 0, 3);
-        SDL_GL_SwapWindow(window);
+        SDL_GL_SwapWindow(ctx.window);
     }
+    #endif
 
-    glDeleteProgram(shader);
+    glDeleteProgram(ctx.shader);
     glDeleteBuffers(1, &vbo);
-    glDeleteVertexArrays(1, &vao);
+    glDeleteVertexArrays(1, &ctx.vao);
     SDL_GL_DeleteContext(context);
-    SDL_DestroyWindow(window);
+    SDL_DestroyWindow(ctx.window);
     SDL_Quit();
     return 0;
 }
